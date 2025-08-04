205d1637cf057c87895ce922ca5a8283cc53476d
Krishi-mehta <mehtakrishi87@gmail.com>
2025-08-04 16:10:31 +0530

    Trigger new deploy

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider,onAuthStateChanged  } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 
 const firebaseConfig = {
   apiKey: "AIzaSyCbR-UsMABkxypLWQR3tmkwJXWoZzIdKQ4",
   authDomain: "filementor-2dae4.firebaseapp.com",
   projectId: "filementor-2dae4",
-  storageBucket: "filementor-2dae4.firebasestorage.app",
+  storageBucket: "filementor-2dae4.appspot.com",
   messagingSenderId: "14603282976",
   appId: "1:14603282976:web:1b1900e19685d116cd6a45",
   measurementId: "G-2V0351BMXN"
